bd8ffb2772382c3c67b48c65e998eb68f2f9b2d5
xukee <kekemixiu@gmail.com>
2024-09-10 10:23:02 +0800

    [commit]:xxx

--- app/src/main/java/com/shuati/xunke/XMainActivity.java
+++ app/src/main/java/com/shuati/xunke/XMainActivity.java
@@ -37,8 +37,8 @@ import java.util.regex.Pattern;
 public class XMainActivity extends AppCompatActivity {
 
     //333新大纲/1中教史 2外教史 3教心 4教原
-    private String lujin = "zhengzhi/xiao/1000/1/";
-    private String index = "00";
+    private String lujin = "zhenti/2024/";
+    private String index = "24";
 
     //如果带颜色的解析，就把这个开关关了,手动输入
     //选择题默认打开这个开关
